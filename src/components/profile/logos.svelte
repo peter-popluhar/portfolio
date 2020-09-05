508804946d5e62e91b46613233b42632d942be05
Peter Popluhar <peter.popluhar@socialbakers.com>
2020-09-05 21:23:07 +0200

    skills icons

--- src/components/profile/logos.svelte
+++ src/components/profile/logos.svelte
@@ -1,36 +1,5 @@
 <style lang="scss">
-  @import "../../style/media-queries";
-  @import "../../style/mixins";
-
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-
-    @include md {
-      justify-content: flex-start;
-    }
-  }
-  
-  li {
-    margin-bottom: 10px;
-    filter: var(--filter);
-    
-    &:not(:last-of-type) {
-      margin-right: 10px;
-    }
-  }
-  
-  img, svg {
-    width: 24px;
-    height: 24px;
-
-    @include lg {
-      width: 36px;
-      height: 36px;
-    }
-  }
+  @import "logos";
 </style>
 
 <script>
@@ -39,6 +8,6 @@
 
 <ul>
   {#each icons as {icon}}
-    <li><img src={icon.url} alt="" role="presentation"></li>
+    <li><img src={icon.url} alt={icon.alt} title={icon.alt} role="presentation"></li>
   {/each}
 </ul>
