1d66c64693b211686c9f983c2038de24bf0eb268
Peter Popluhar <popluhar.peter@gmail.com>
2020-10-02 21:51:49 +0200

    Lazy load of portfolio items

--- src/components/portfolio/porfolio-item.svelte
+++ src/components/portfolio/porfolio-item.svelte
@@ -1,38 +1,47 @@
 <script>
   import Labels from "../labels/labels.svelte";
   import { isHeaderFloating } from "../../stores";
-  import ImageMain from '../image/image.svelte'
-  import PrismicDOM from 'prismic-dom';
-  import { Client, linkResolver } from '../../../prismic-config';
-  import {gsap} from 'gsap'
+  import ImageMain from "../image/image.svelte";
+  import PrismicDOM from "prismic-dom";
+  import { Client, linkResolver } from "../../../prismic-config";
+  import { gsap } from "gsap";
   import { ScrollTrigger } from "gsap/ScrollTrigger";
-  import {onMount} from 'svelte'
-  
+  import { onMount } from "svelte";
+  import Lazy from "svelte-lazy";
+
   export let post;
-  const {data} = post
-  const {title, position, tags, year, bg_image, sliced_image} = data
+  const { data } = post;
+  const { title, position, tags, year, bg_image, sliced_image } = data;
 
   gsap.registerPlugin(ScrollTrigger);
   onMount(() => {
     let boxes = gsap.utils.toArray(".item");
 
-    boxes.forEach((box) => {
-      const animBoxes = gsap.fromTo(box, {autoAlpha: 0.1}, {duration: .2, autoAlpha: 1});
+    boxes.forEach(box => {
+      const animBoxes = gsap.fromTo(
+        box,
+        { autoAlpha: 0.1 },
+        { duration: 0.2, autoAlpha: 1 }
+      );
       ScrollTrigger.create({
         trigger: box,
         animation: animBoxes,
         scrub: 1,
-        start: 'top 70%',
-        end: 'bottom 60%',
+        start: "top 70%",
+        end: "bottom 60%"
       });
-      
-      const animHr = gsap.fromTo(box.nextElementSibling, {width: 0}, {width: '100%'});
+
+      const animHr = gsap.fromTo(
+        box.nextElementSibling,
+        { width: 0 },
+        { width: "100%" }
+      );
       ScrollTrigger.create({
         trigger: box,
         animation: animHr,
         scrub: true,
-        start: 'top 40%',
-        end: 'bottom 40%',
+        start: "top 40%",
+        end: "bottom 40%"
       });
     });
   });
@@ -42,24 +51,28 @@
   @import "porfolio-item";
 </style>
 
-<a rel="prefetch" href={linkResolver(post)} class="portfolio-item item">
-  <h2 class={$isHeaderFloating ? 'header-is-floating' : ''}>{PrismicDOM.RichText.asText(title)}</h2>
-  <div class="content-holder">
-    <h2>{PrismicDOM.RichText.asText(title)}</h2>
-    <h3>{PrismicDOM.RichText.asText(position)}</h3>
-    <p>{PrismicDOM.RichText.asText(year)}</p>
-    <Labels tags={tags} />
-  </div>
-  <div class="frame">
-    <ImageMain image={bg_image.url} />
-    <div class="slices">
-      {#each Array(10) as _, i}
-        <div
-          class="slice slice-{i + 1}"
-          style="background-image: url({sliced_image.url})" />
-      {/each}
+<Lazy height={300}>
+  <a rel="prefetch" href={linkResolver(post)} class="portfolio-item item">
+    <h2 class={$isHeaderFloating ? 'header-is-floating' : ''}>
+      {PrismicDOM.RichText.asText(title)}
+    </h2>
+    <div class="content-holder">
+      <h2>{PrismicDOM.RichText.asText(title)}</h2>
+      <h3>{PrismicDOM.RichText.asText(position)}</h3>
+      <p>{PrismicDOM.RichText.asText(year)}</p>
+      <Labels {tags} />
     </div>
+    <div class="frame">
+      <ImageMain image={bg_image.url} />
+      <div class="slices">
+        {#each Array(10) as _, i}
+          <div
+            class="slice slice-{i + 1}"
+            style="background-image: url({sliced_image.url})" />
+        {/each}
+      </div>
 
-  </div>
-</a>
-<hr />
+    </div>
+  </a>
+  <hr />
+</Lazy>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,20 @@
 <script context="module">
-  import Prismic from 'prismic-javascript';
-  import PrismicDOM from 'prismic-dom';
-  import { Client } from '../../prismic-config.js';
+  import Prismic from "prismic-javascript";
+  import PrismicDOM from "prismic-dom";
+  import { Client } from "../../prismic-config.js";
 
   let homepage = null;
   let posts = null;
 
   export async function preload({ params, query }) {
-    homepage = await Client.getSingle('homepage');
+    homepage = await Client.getSingle("homepage");
 
     const postResponse = await Client.query(
-            Prismic.Predicates.at('document.type', 'article'),
-            { orderings: '[my.article.order]' }
-    )
+      Prismic.Predicates.at("document.type", "article"),
+      { orderings: "[my.article.order]" }
+    );
 
-    posts = postResponse.results
+    posts = postResponse.results;
 
     if (homepage && posts) {
       return { homepage, posts };
@@ -26,7 +26,7 @@
 
 <script>
   import { fade } from "svelte/transition";
-  import {fadeIn} from '../utils/page-transition'
+  import { fadeIn } from "../utils/page-transition";
   import Portfolio from "../components/portfolio/porfolio-list.svelte";
   import Profile from "../components/profile/profile.svelte";
   import Quota from "../components/quota/quota.svelte";
@@ -34,8 +34,8 @@
 
   export let homepage;
   export let posts;
-  const {data} = homepage
-  const {title, quota_author, quota_text} = homepage.data
+  const { data } = homepage;
+  const { title, quota_author, quota_text } = homepage.data;
 </script>
 
 <svelte:head>
@@ -45,11 +45,11 @@
 <main in:fade={fadeIn}>
   <Profile data={homepage.data} />
   <Timeline />
-  <Portfolio posts={posts} />
+  <Portfolio {posts} />
   <Quota author={quota_author} text={quota_text} />
   <p class="text--sm">
-    * This site is build on Sapper for SSR, FE powered by Svelte and Scss. Animations with GSAP.
-    Content is fetched from Prismic Headless CMS.
+    * This site is build on Sapper for SSR, FE powered by Svelte and Scss.
+    Animations with GSAP. Content is fetched from Prismic Headless CMS.
     Deployment via github, vercel.com and webhooks.
   </p>
 </main>
